644ce632884f9c38b2910e32b3c7cfd4501d6a69
pavel ilin <refugeebase@gmail.com>
2022-02-02 12:26:07 -0500

    'init'

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [mediumData, setMediumData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@pavel-ilin`
+    )
+      .then(res => res.json())
+      .then(response => {
+        setMediumData(response.items);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const renderArticles = (data) => {
+      return data.map((article) => {
+        console.log(article)
+        return  <article key={article.pubDate}>
+                  <img width="20%" src={article.thumbnail} />
+                  <h3><a href={article.link}>{article.title}</a></h3>
+                </article>
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2 className="App-header">Pavel Ilin's blog</h2>
+      </div>
+      <div className="App">
+          {!isLoading && renderArticles(mediumData)}
+      </div>
     </div>
   );
 }
